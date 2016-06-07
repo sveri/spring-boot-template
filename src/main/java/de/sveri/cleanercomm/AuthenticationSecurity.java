3174dd34cdcbb7c4a197f44fc163c6168132ba72
sveri <sveri80@gmail.com>
2016-06-07 22:41:01 +0200

    home index

--- src/main/java/de/sveri/cleanercomm/AuthenticationSecurity.java
+++ src/main/java/de/sveri/cleanercomm/AuthenticationSecurity.java
@@ -6,14 +6,14 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 
-@Order(Ordered.HIGHEST_PRECEDENCE)
-@Configuration
-public class AuthenticationSecurity extends GlobalAuthenticationConfigurerAdapter {
-
-	@Override
-	public void init(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN", "USER").and().withUser("user")
-				.password("user").roles("USER");
-	}
-
-}
+//@Order(Ordered.HIGHEST_PRECEDENCE)
+//@Configuration
+//public class AuthenticationSecurity extends GlobalAuthenticationConfigurerAdapter {
+//
+//	@Override
+//	public void init(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN", "USER").and().withUser("user")
+//				.password("user").roles("USER");
+//	}
+//
+//}
