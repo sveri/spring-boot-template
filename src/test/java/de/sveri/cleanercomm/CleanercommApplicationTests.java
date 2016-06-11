33444da8b0f86e8a90d4ada06a241016b64010d1
sveri <sveri80@gmail.com>
2016-06-11 11:15:18 +0200

    cleanup

--- src/test/java/de/sveri/cleanercomm/CleanercommApplicationTests.java
+++ src/test/java/de/sveri/cleanercomm/CleanercommApplicationTests.java
@@ -2,12 +2,8 @@ package de.sveri.cleanercomm;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-
-import de.sveri.cleanercomm.CleanercommApplication;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
