576742cd2fa4980fdea20e8ea5e61b75649057ff
sveri <sveri@sveri.de>
2016-06-11 10:37:58 +0200

    trying to login again

--- src/main/java/de/sveri/cleanercomm/ApplicationSecurity.java
+++ src/main/java/de/sveri/cleanercomm/ApplicationSecurity.java
@@ -1,41 +1,50 @@
 package de.sveri.cleanercomm;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import de.sveri.cleanercomm.service.UserService;
+
 @Configuration
-@EnableOAuth2Sso
 //@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 
-//	@Override
-//	public void configure(WebSecurity web) throws Exception {
-//		web.ignoring().antMatchers("/");
-////		super.configure(web);
-//	}
+    @Autowired
+    private UserService userService;
+    
+    @Value("${app.secret}")
+    private String applicationSecret;
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.antMatcher("/**").authorizeRequests()
-		.antMatchers("/", "/login**").permitAll()
-		.anyRequest().authenticated();
+		.antMatchers("/user/register").permitAll()
+        .antMatchers("/user/activate").permitAll()
+        .antMatchers("/user/activation-send").permitAll()
+        .antMatchers("/user/reset-password").permitAll()
+        .antMatchers("/user/reset-password-change").permitAll()
+        .antMatchers("/user/list").access("hasRole('ROLE_ADMIN')")
+        .antMatchers("/user/autologin").access("hasRole('ROLE_ADMIN')")
+        .antMatchers("/user/delete").access("hasRole('ROLE_ADMIN')")
+		.anyRequest().authenticated()
+		.and()
+        .formLogin().loginPage("/login").failureUrl("/login?error").permitAll()
+	    .and()
+	        .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login")
+	    .and()
+	        .rememberMe().key(applicationSecret)
+	        .tokenValiditySeconds(31536000);
 	}
-
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http.authorizeRequests().
-//		antMatchers("/", "/user/register").permitAll().
-////		antMatchers("/admin").hasRole("Admin").
-//		antMatchers("/login").permitAll().anyRequest().fullyAuthenticated().and().formLogin()
-//				.loginPage("/login").failureUrl("/login?error").and().logout()
-//				.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).and().exceptionHandling()
-//				.accessDeniedPage("/access?error");
-//	}
-
+	
+    
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(new BCryptPasswordEncoder());
+    }
 }
